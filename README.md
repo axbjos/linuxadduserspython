667924dc101564e94242d8e39cd2c3bbf64eb92a
Joe Axberg <axbjos@dunwoody.edu>
2022-10-06 21:01:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Linux Add Users Script 
---
+++ createusers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# a comment
+import sys
+import re
+
+def main():
+
+
+    for line in sys.stdin:
+        m = re.match('^\s*#.*$',line)
+        if m:
+            continue
+        print(line)
+
+    print("got to here")
+
+main()
